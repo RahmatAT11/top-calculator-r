20e3b0c1a59b4ec8dfb4015467afe10a1f258d52
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2023-04-09 09:06:36 +0700

    Add 4 function to do some basic calculation
    - add
    - substract
    - multiply
    - divide

---
+++ script.js
@@ -0,0 +1,13 @@
+// Create function for add, substract, multiply, divide
+const add = (num1, num2) => num1 + num2;
+
+const substract = (num1, num2) => num1 - num2;
+
+const multiply = (num1, num2) => num1 * num2;
+
+const divide = (num1, num2) => num1 / num2;
+
+console.log(add(1, 2));
+console.log(substract(1, 2));
+console.log(multiply(1, 2));
+console.log(divide(1, 2));
