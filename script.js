25e4c7f54d98456d9e1cc6949c96fc96f4adbe13
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2023-04-09 09:13:19 +0700

    Create operate function
    - This function takes 2 number as parameter,
    - and a operator string
    - then return the calculation as a number

--- script.js
+++ script.js
@@ -7,7 +7,32 @@ const multiply = (num1, num2) => num1 * num2;
 
 const divide = (num1, num2) => num1 / num2;
 
+const operate = (num1, num2, operator) => {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+
+        case "-":
+            return substract(num1, num2);
+
+        case "*":
+        case "x":
+            return multiply(num1, num2);
+
+        case "/":
+            return divide(num1, num2);
+    
+        default:
+            return "There is no such operation"
+    }
+};
+
+let number1 = 0;
+let number2 = 0;
+let operator = "";
+
 console.log(add(1, 2));
 console.log(substract(1, 2));
 console.log(multiply(1, 2));
 console.log(divide(1, 2));
+console.log(operate(1, 2, "-"))
