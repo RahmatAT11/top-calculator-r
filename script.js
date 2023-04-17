bc6d7bd6e8fccdf8fa92f556d81f5b8125b51c4d
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2023-04-17 10:09:27 +0700

    Operation display worked

--- script.js
+++ script.js
@@ -2,12 +2,17 @@
 let number1 = "";
 let number2 = "";
 let operator = "";
+let changeNumDisplay = false;
+let stopOperatorChange = false;
 
 const contOperation = document.querySelector(".container-operation");
 
 let displayNum1 = undefined;
 let displayOperation = undefined;
 let displayNum2 = undefined;
+let numBtn = undefined;
+let oprtrBtn = undefined;
+let oprtrBtnLong = undefined;
 
 const add = (num1, num2) => num1 + num2;
 
@@ -56,17 +61,35 @@ const createDisplayOperator = (className, typeOperator = "+") => {
 }
 
 const showNumber = (e) => {
-    if (typeof displayNum1 === "undefined") {
-        displayNum1 = createDisplayNumber("display-num", e.target.id);
-        contOperation.appendChild(displayNum1);
-
-        displayNum1.id += number1;
-        number1 += e.target.id;
-    } else {
-        number1 += e.target.id;
-
-        displayNum1.textContent = number1;
-        displayNum1.id = number1;
+    switch (changeNumDisplay) {
+        case true:
+            if (typeof displayNum2 === "undefined") {
+                displayNum2 = createDisplayNumber("display-num", e.target.id);
+                contOperation.appendChild(displayNum2);
+        
+                number2 += e.target.id;
+                stopOperatorChange = true;
+            } else {
+                number2 += e.target.id;
+        
+                displayNum2.textContent = number2;
+                displayNum2.id = number2;
+            }
+            break;
+    
+        default:
+            if (typeof displayNum1 === "undefined") {
+                displayNum1 = createDisplayNumber("display-num", e.target.id);
+                contOperation.appendChild(displayNum1);
+        
+                number1 += e.target.id;
+            } else {
+                number1 += e.target.id;
+        
+                displayNum1.textContent = number1;
+                displayNum1.id = number1;
+            }
+            break;
     }
 
     // if (typeof displayNum2 === "undefined") {
@@ -82,16 +105,21 @@ const showOperator = (e) => {
     if (typeof displayOperation === "undefined") {
         displayOperation = createDisplayOperator("display-operation", e.target.id);
         contOperation.appendChild(displayOperation)
-    } else {
+        changeNumDisplay = true;
+    } else if (stopOperatorChange === false){
         operator = e.target.id;
         displayOperation.textContent = operator;
+        displayOperation.id = operator;
+
+        console.log("GOD")
+        numBtn.forEach((btn) => btn.removeEventListener("click", console.log(btn)))
     }
 }
 
 const prepareCalculator = () => {
-    const numBtn = Array.from(document.querySelectorAll(".calculator-btn-blue-verylight"));
-    const oprtrBtn = Array.from(document.querySelectorAll(".calculator-btn-blue"));
-    const oprtrBtnLong = document.querySelector(".calculator-btn-blue-long");
+    numBtn = Array.from(document.querySelectorAll(".calculator-btn-blue-verylight"));
+    oprtrBtn = Array.from(document.querySelectorAll(".calculator-btn-blue"));
+    oprtrBtnLong = document.querySelector(".calculator-btn-blue-long");
     
     oprtrBtn.push(oprtrBtnLong);
 
